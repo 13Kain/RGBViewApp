ad460b6819bf0010e4827adfe5a849efb5019e83
Nikita Yashin <n.yashin88@icloud.com>
2022-03-04 17:34:14 +0300

    starting project

--- RGBViewApp/ViewController.swift
+++ RGBViewApp/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var blueValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var redValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
